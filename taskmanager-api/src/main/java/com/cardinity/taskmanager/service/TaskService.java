7c401081f8eed2953e8d33d3767870cd4c6d0134
ahadahmed <ahadahmed.career@gmail.com>
2021-02-19 21:29:35 +0600

    Implemented service to update task status. This commit resolve following issue, resolved#10

--- taskmanager-api/src/main/java/com/cardinity/taskmanager/service/TaskService.java
+++ taskmanager-api/src/main/java/com/cardinity/taskmanager/service/TaskService.java
@@ -4,13 +4,16 @@ import com.cardinity.taskmanager.dao.TaskDAO;
 import com.cardinity.taskmanager.dto.TaskDto;
 import com.cardinity.taskmanager.entity.Project;
 import com.cardinity.taskmanager.entity.Task;
+import com.cardinity.taskmanager.entity.TaskStatus;
 import com.cardinity.taskmanager.util.TaskUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.NotNull;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 /**
  * @author ahadahmed
@@ -30,7 +33,7 @@ public class TaskService {
     }
 
 
-    public TaskDto createTask(TaskDto taskDto) {
+    public TaskDto createTask(@NotNull TaskDto taskDto) {
         if (StringUtils.isEmpty(StringUtils.trim(taskDto.getTaskDescription()))) {
             throw new InvalidTaskException("task description is empty");
         }
@@ -48,5 +51,33 @@ public class TaskService {
         task = this.taskRepository.save(task);
         taskDto = taskUtil.convertEntityToDto(task);
         return taskDto;
+    }
+
+    public TaskDto updateTaskStatus(@NotNull long taskId, @NotNull TaskStatus newTaskStatus){
+        if(newTaskStatus == null){
+            throw new InvalidTaskException("invalid task status");
+        }
+        Task existingTask;
+        try {
+            existingTask = this.getTaskById(taskId);
+        }catch (NoSuchElementException  | InvalidDataAccessApiUsageException ex){
+            throw new NoSuchElementException("task not found with id " + taskId);
+        }
+        if(existingTask.getTaskStatus() == TaskStatus.CLOSED){
+            throw new InvalidTaskException("can not update already closed task");
+        }
+        existingTask.setTaskStatus(newTaskStatus);
+        existingTask = this.taskRepository.save(existingTask);
+        TaskDto taskDto =this.taskUtil.convertEntityToDto(existingTask);
+
+        return taskDto;
+    }
+
+    public Task getTaskById(@NotNull long taskId){
+        Optional<Task> task = this.taskRepository.findById(taskId);
+        if(task.isEmpty()){
+            throw new NoSuchElementException("task not found with id "+ taskId);
+        }
+        return task.get();
     }
 }

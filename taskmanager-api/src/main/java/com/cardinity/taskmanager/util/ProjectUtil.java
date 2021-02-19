73cc099090205f7ae121071cefa609a42207cca3
ahadahmed <ahadahmed.career@gmail.com>
2021-02-19 13:47:23 +0600

    Added ProjectUtil to convert Entity to DTO and vise versa.

---
+++ taskmanager-api/src/main/java/com/cardinity/taskmanager/util/ProjectUtil.java
@@ -0,0 +1,29 @@
+package com.cardinity.taskmanager.util;
+
+import com.cardinity.taskmanager.controllers.rest.ProjectDTO;
+import com.cardinity.taskmanager.entity.Project;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ahadahmed
+ * @see <a href="#"> see this</a>
+ */
+@Component
+public class ProjectUtil {
+    private ModelMapper modelMapper;
+
+    @Autowired
+    public ProjectUtil(ModelMapper modelMapper){
+        this.modelMapper = modelMapper;
+    }
+
+    public ProjectDTO convertEntityToDto(Project project){
+        return this.modelMapper.map(project, ProjectDTO.class);
+    }
+
+    public Project convertDtoToEntity(ProjectDTO projectDTO){
+        return this.modelMapper.map(projectDTO, Project.class);
+    }
+}

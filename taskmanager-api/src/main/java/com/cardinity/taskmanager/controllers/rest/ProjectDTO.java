013a02bb85a5f8e205ab6aff250848794a0348b0
ahadahmed <ahadahmed.career@gmail.com>
2021-02-21 14:03:35 +0600

    Update ProjectDTO and introduced different visibility for different http Method(POST,PUT).

--- taskmanager-api/src/main/java/com/cardinity/taskmanager/controllers/rest/ProjectDTO.java
+++ taskmanager-api/src/main/java/com/cardinity/taskmanager/controllers/rest/ProjectDTO.java
@@ -1,29 +1,41 @@
 package com.cardinity.taskmanager.controllers.rest;
 
 import com.cardinity.taskmanager.entity.ProjectStatus;
+import com.cardinity.taskmanager.entity.Task;
+import com.fasterxml.jackson.annotation.*;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author ahadahmed
  * @see <a href="#"> see this</a>
  */
+//@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class ProjectDTO {
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private long id;
-    @NotBlank
+//    @NotBlank
+    @Schema(required = true, description = "The name of the project to be created")
+    @JsonView(value = {View.HttpMethodView.POST.class, View.HttpMethodView.PUT.class})
     private String projectName;
-//    org.hibernate.dialect.MySQL8Dialect
 
     @Length(max = 5, message = "project description max length must be 5 or less.")
     private String description;
-
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private LocalDateTime created;
 
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    private LocalDateTime updated;
+
+    @JsonView(value = {View.HttpMethodView.PUT.class})
     private ProjectStatus projectStatus;
+
+
+    @JsonManagedReference
+    private List<Task> tasks;
 
     public ProjectDTO(){}
 
@@ -59,11 +71,27 @@ public class ProjectDTO {
         this.created = created;
     }
 
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+
     public ProjectStatus getProjectStatus() {
         return projectStatus;
     }
 
     public void setProjectStatus(ProjectStatus projectStatus) {
         this.projectStatus = projectStatus;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }

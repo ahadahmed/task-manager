68ad3c1359baa8de8a179a45a1a29e41196cc440
ahadahmed <ahadahmed.career@gmail.com>
2021-02-21 13:57:14 +0600

    Implemented util function to convert list of task entity to list of task dto.

--- taskmanager-api/src/main/java/com/cardinity/taskmanager/dao/TaskDAO.java
+++ taskmanager-api/src/main/java/com/cardinity/taskmanager/dao/TaskDAO.java
@@ -1,7 +1,16 @@
 package com.cardinity.taskmanager.dao;
 
+import com.cardinity.taskmanager.entity.Project;
 import com.cardinity.taskmanager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
-public interface TaskDAO extends PagingAndSortingRepository<Task, Long> {
+import java.util.List;
+
+@Repository
+public interface TaskDAO extends JpaRepository<Task, Long> {
+
+    List<Task> findAllByProject(Project project);
 }

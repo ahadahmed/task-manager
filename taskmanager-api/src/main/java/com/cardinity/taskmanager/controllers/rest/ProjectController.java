1dee160787fe2fcf130e3813f51790e11999eddb
ahadahmed <ahadahmed.career@gmail.com>
2021-02-22 11:39:42 +0600

    Introduce authorization on all project rest APIs.

--- taskmanager-api/src/main/java/com/cardinity/taskmanager/controllers/rest/ProjectController.java
+++ taskmanager-api/src/main/java/com/cardinity/taskmanager/controllers/rest/ProjectController.java
@@ -13,6 +13,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.parameters.P;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,7 +46,7 @@ public class ProjectController {
             ,responseCode = "200"
             , description = "Success response")
     @GetMapping("/project")
-    @PreAuthorize("hasRole('ROLE_USER')")
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ADMIN')")
     public List<Project> getAll() {
         ProjectDTO p = new ProjectDTO();
         List<Project> projects = this.projectService.getAll();
@@ -54,6 +55,7 @@ public class ProjectController {
     }
 
     @Operation(summary = "Get a specific Project info", description = "API for getting information of a specific project by projectId")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping("/project/{projectId}")
     public ProjectDTO getProject(@PathVariable("projectId") @Min(value = 1, message = "project id must be greater than 0") long projectId) {
         ProjectDTO p = new ProjectDTO();
@@ -64,10 +66,10 @@ public class ProjectController {
     }
 
     @Operation(summary = "Get all task list of a specific Project", description = "API for getting task list of a specific project by projectId")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping("/project/{projectId}/tasks")
     public List<Task> getProjectTasks(@PathVariable("projectId") @Min(value = 1, message = "project id must be greater than 0") long projectId) {
         ProjectDTO p = new ProjectDTO();
-        p.setProjectName("test-proj");
         Project project = this.projectService.getProject(projectId);
         p = this.projectUtil.convertEntityToDto(project);
         return p.getTasks();
@@ -83,7 +85,7 @@ public class ProjectController {
     )}
             ,responseCode = "200"
             , description = "Success response")
-
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @PostMapping("/project")
     public ProjectDTO create(@RequestBody @JsonView(value = View.HttpMethodView.POST.class)
                                  @Valid ProjectDTO projectDTO){
@@ -92,6 +94,7 @@ public class ProjectController {
     }
 
     @Operation(summary = "Delete Project", description = "API for delete a project")
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @DeleteMapping("/project/{projectId}")
     public ProjectDTO removeProject(@PathVariable("projectId") @Min(value = 1, message = "project id must be greater than 0") long projectId){
         ProjectDTO projectDTO = this.projectService.deleteProject(projectId);

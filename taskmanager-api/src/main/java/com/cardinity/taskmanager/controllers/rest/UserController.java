69ba2091a11ececc6d499e35aff6d6ebe1dc5d9d
ahadahmed <ahadahmed.career@gmail.com>
2021-02-22 10:34:15 +0600

    secure REST end points by integrating spring security into project. As a result REST API's now need JWT AUTH token as Authorization header for accessing.

---
+++ taskmanager-api/src/main/java/com/cardinity/taskmanager/controllers/rest/UserController.java
@@ -0,0 +1,50 @@
+package com.cardinity.taskmanager.controllers.rest;
+
+import com.cardinity.taskmanager.dto.LoginDto;
+import com.cardinity.taskmanager.entity.User;
+import com.cardinity.taskmanager.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpServerErrorException;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signin")
+    public String login(@RequestBody @Valid LoginDto loginDto) {
+       return userService.signin(loginDto.getUsername(), loginDto.getPassword()).orElseThrow(()->
+               new BadCredentialsException("Bad credentials"));
+    }
+
+    @PostMapping("/signup")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User signup(@RequestBody @Valid LoginDto loginDto){
+        return userService.signup(loginDto.getUsername(), loginDto.getPassword(), loginDto.getFirstName(),
+                loginDto.getLastName()).orElseThrow(() -> new HttpServerErrorException(HttpStatus.BAD_REQUEST,"User already exists"));
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<User> getAllUsers() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return userService.getAll();
+    }
+
+}
